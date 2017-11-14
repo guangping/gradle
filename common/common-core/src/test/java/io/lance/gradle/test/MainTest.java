903bebad8d28986b4a738bfbffc6de1765f25e63
lance <mgp@ebidding.com.cn>
2017-11-14 16:58:10 +0800

    调整

---
+++ common/common-core/src/test/java/io/lance/gradle/test/MainTest.java
@@ -0,0 +1,24 @@
+package io.lance.gradle.test;
+
+import org.testng.annotations.Test;
+import org.testng.collections.Lists;
+
+import java.util.List;
+
+/**
+ * @author Lance.
+ * @time: 2017-11-14 14:54
+ * @desc:
+ */
+public class MainTest {
+
+
+    @Test
+    public void run(){
+        List<String> items= Lists.newArrayList();
+        items.add("1");
+        items.add("2");
+
+        System.out.println(items.contains("1"));
+    }
+}

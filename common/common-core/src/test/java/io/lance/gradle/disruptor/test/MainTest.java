f8daad981d4ba35e69726e21aca879bc5c17aaac
lance <guangping.sony@gmail.com>
2018-03-23 15:50:09 +0800

    调整发布

--- common/common-core/src/test/java/io/lance/gradle/disruptor/test/MainTest.java
+++ common/common-core/src/test/java/io/lance/gradle/disruptor/test/MainTest.java
@@ -7,6 +7,7 @@ import io.lance.gradle.common.core.spring.disruptor.generic.GenericEventFactory;
 import io.lance.gradle.common.core.spring.disruptor.generic.GenericEventHandler;
 import io.lance.gradle.common.core.spring.disruptor.generic.GenericEventProducer;
 
+import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -25,17 +26,27 @@ public class MainTest {
         Disruptor<GenericEvent<String>> disruptor = new Disruptor(eventFactory, bufferSize,
                 Executors.defaultThreadFactory());
 
-        disruptor.handleEventsWith(new GenericEventHandler("1"));
+        disruptor.handleEventsWith(new GenericEventHandler<String>("1"));
 
         disruptor.start();
 
-        RingBuffer<GenericEvent<String>> ringBuffer=disruptor.getRingBuffer();
+        RingBuffer<GenericEvent<String>> ringBuffer = disruptor.getRingBuffer();
 
-        GenericEventProducer<String> producer=new GenericEventProducer(ringBuffer);
-        producer.publish("ssss");
+        GenericEventProducer<String> producer = new GenericEventProducer(ringBuffer);
+        //producer.publish("ssss");
 
 
-
+        ExecutorService es = Executors.newFixedThreadPool(100);
+        for (int i = 0; i < 10; i++) {
+            es.submit(new Runnable() {
+                public void run() {
+                    for (long l = 0; l < 20; l++) {
+                        producer.publish(UUID.randomUUID().toString());
+                    }
+                }
+            });
+        }
+        System.out.println("end.....");
 
     }
 
